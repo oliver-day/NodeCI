32c2ec254ca78f0d43e27b38ab93bf4b439d9d5a
Oliver Day <oliver.day@usc.edu>
2019-11-30 19:28:56 -0800

    travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://oday:DT1sukIGC97Etwir@cluster0-shard-00-00-rvdzo.mongodb.net:27017,cluster0-shard-00-01-rvdzo.mongodb.net:27017,cluster0-shard-00-02-rvdzo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
